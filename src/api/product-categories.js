646a2ebf66cddb0a076b3886e67d5b0a3e6eb314
Tihomir Kehayov <kehayov89@gmail.com>
2023-11-14 00:22:50 +0200

    add product page

--- src/api/product-categories.js
+++ src/api/product-categories.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { BASE_URL } from "../config";
 
 export class ProductCategoriesApi {
   static async fetchCategories() {
-    const response = await axios.get(`/categories`);
+    const response = await axios.get(`${BASE_URL}/categories`);
 
     return response.data;
   }
