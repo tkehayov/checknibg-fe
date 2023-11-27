bb41a506cec79e72aeb7a7586218382b6495b68e
Tihomir Kehayov <kehayov89@gmail.com>
2023-11-27 20:33:59 +0200

    create categories dropdown in Header

---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header/Header";
+
+export function CategoryPage() {
+  return (
+    <div>
+      <Header />
+      <h1>hello from category</h1>
+    </div>
+  );
+}
