2a560d20b4f49d9813f64092c4f77ee468e26490
Tihomir Kehayov <kehayov89@gmail.com>
2023-12-11 15:53:22 +0200

    7-create-reusable-pagination-component

---
+++ src/components/Pagination/PaginationComponent.jsx
@@ -0,0 +1,39 @@
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+
+export function PaginationComponent({
+  elements,
+  handlePageChange,
+  isPageCleared,
+}) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parseInt(searchParams.get("page"))
+    ? parseInt(searchParams.get("page"))
+    : 1;
+
+  function handlePageChange_(event, page) {
+    handlePageChange(page);
+    setSearchParams({ page: page });
+  }
+
+  useEffect(() => {
+    if (isPageCleared) {
+      handlePageChange(1);
+      setSearchParams({ page: 1 });
+    }
+  }, [isPageCleared]);
+
+  return (
+    <Stack>
+      <Pagination
+        count={elements.totalPages}
+        shape="rounded"
+        color="primary"
+        onChange={handlePageChange_}
+        page={currentPage}
+      />
+    </Stack>
+  );
+}
