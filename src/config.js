510ae41eec50424ec54d186eba4cec41a1528e3a
Tihomir Kehayov <kehayov89@gmail.com>
2023-12-20 22:56:24 +0200

    11-create-product-page

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ const BASE_URL = "/api";
 const PAGES_URL = { home: "/", category: "/category", product: "/product" };
 const IMAGES_URL = "/images";
 const PRODUCTS_IMAGES_URL = IMAGES_URL + "/products";
-export { BASE_URL, PAGES_URL, PRODUCTS_IMAGES_URL };
+const MERCHANTS_IMAGES_URL = IMAGES_URL + "/merchants";
+
+export { BASE_URL, PAGES_URL, PRODUCTS_IMAGES_URL, MERCHANTS_IMAGES_URL };
