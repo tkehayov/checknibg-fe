855040ca399d1d495f703942a7944f94e08116c6
Tihomir Kehayov <kehayov89@gmail.com>
2024-01-06 21:28:46 +0200

    15 create breadcrumbs for pages (#16)
    * 15-create-breadcrumbs-for-pages

---
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,35 @@
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+
+export function BreadCrumbs({ breadcrumbs }) {
+  return (
+    <Stack spacing={2}>
+      {breadcrumbs && (
+        <Breadcrumbs separator="›" aria-label="breadcrumb">
+          {breadcrumbs.map((breadcrumb) => {
+            if (breadcrumb.href) {
+              return (
+                <Link
+                  underline="hover"
+                  key={breadcrumb.key}
+                  color="inherit"
+                  href={breadcrumb.href}
+                >
+                  {breadcrumb.name}
+                </Link>
+              );
+            } else {
+              return (
+                <Typography key={breadcrumb.key} color="text.primary">
+                  {breadcrumb.name}
+                </Typography>
+              );
+            }
+          })}
+        </Breadcrumbs>
+      )}
+    </Stack>
+  );
+}
