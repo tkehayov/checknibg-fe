f1fdd90ceffcee1df2c80c676bf457bf772bef47
Tihomir Kehayov <kehayov89@gmail.com>
2025-03-12 19:55:48 +0200

    46-display-total-product-click-amount-per-month

---
+++ cheknibg-merchant-admin/src/api/analyze.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { API_URLS } from "../config";
+
+export class AnalyzeApi {
+  static async getMonthlyTotalProducts(merchantId, month) {
+    const response = await axios
+      .get(
+        `${API_URLS.finance}/product-counter/month/${month}?merchantId=${merchantId}`
+      )
+      .then((res) => {
+        return res;
+      })
+      .catch((error) => {
+        if (error) {
+          return error.response;
+        }
+      });
+
+    return response.data;
+  }
+}
