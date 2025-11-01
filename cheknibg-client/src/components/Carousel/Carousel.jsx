d611fc167526cf6b31f1cb1dbd5d6ea9c8b66cf5
Tihomir Kehayov <kehayov89@gmail.com>
2025-11-01 14:09:07 +0200

    64-install-tag-manager

--- cheknibg-client/src/components/Carousel/Carousel.jsx
+++ cheknibg-client/src/components/Carousel/Carousel.jsx
@@ -1,38 +1,71 @@
 import Carousel from "react-material-ui-carousel";
-import { Button } from "@mui/material";
+import { Button, Grid, Box, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 export function CarouselAd() {
-  var items = [
-    {
-      name: "Random Name #1",
-      img: "laptops.jpg",
-      description: "Probably the most random thing you have ever seen!",
-    },
-    {
-      name: "Random Name #2",
-      img: "slider2.jpg",
-      description: "Hello World!",
-    },
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
+  const isSmUp = useMediaQuery(theme.breakpoints.up("sm"));
+  const numberPerSlide = isMdUp ? 3 : isSmUp ? 2 : 1;
+
+  // TODO: Replace with real images
+  const items = [
+    "laptops.jpg",
+    "slider2.jpg",
+    "slider3.jpg",
+    "laptops.jpg",
+    "laptops.jpg",
   ];
 
+  const slides = [];
+  for (let i = 0; i < items.length; i += numberPerSlide) {
+    const chunk = items.slice(i, i + numberPerSlide).map((img) => ({ img }));
+    slides.push(chunk);
+  }
+
   return (
-    <Carousel>
-      {items.map((item, i) => (
-        <Item key={i} item={item} />
+    <Carousel navButtonsAlwaysVisible animation="slide" indicators={false}>
+      {slides.map((slide, i) => (
+        <Slide key={i} images={slide} numberPerSlide={numberPerSlide} />
       ))}
     </Carousel>
   );
 }
-function Item(props) {
-  return (
-    <div>
-      <h2>
-        {props.item.name} {props.item.img}
-      </h2>
-      <img src={`/assets/slider/${props.item.img}`} alt={`${props.item.img}`} />
-      <p>{props.item.description}</p>
 
-      <Button className="CheckButton">Check it out!</Button>
-    </div>
+function Slide({ images, numberPerSlide }) {
+  const mdSize = Math.max(1, Math.floor(12 / numberPerSlide));
+  return (
+    <Box sx={{ padding: 2 }}>
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
+        {images.map((it, idx) => (
+          <Grid item xs={12} sm={6} md={mdSize} key={idx}>
+            <Box
+              sx={{
+                borderRadius: 1,
+                overflow: "hidden",
+                boxShadow: 1,
+                textAlign: "center",
+                bgcolor: "background.paper",
+                height: 220,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src={`/assets/slider/${it.img}`}
+                alt={it.img}
+                style={{ maxWidth: "100%", maxHeight: 140, objectFit: "cover" }}
+              />
+              <Box sx={{ px: 1, pt: 1 }}>
+                <Button size="small" variant="contained" color="primary">
+                  Check it out!
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
