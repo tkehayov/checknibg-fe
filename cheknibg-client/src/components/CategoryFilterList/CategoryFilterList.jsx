6431a4b3501bd24302a82348755b17b39566704c
Tihomir Kehayov <kehayov89@gmail.com>
2026-01-06 14:46:43 +0200

    99-add-price-filters (#100)

--- cheknibg-client/src/components/CategoryFilterList/CategoryFilterList.jsx
+++ cheknibg-client/src/components/CategoryFilterList/CategoryFilterList.jsx
@@ -1,31 +1,17 @@
-import { ProductCategoriesApi } from "../../api/product-categories";
-import { useState, useEffect } from "react";
 import FormGroup from "@mui/material/FormGroup";
 import { CategoryGroupListItem } from "../CategoryGroupListItem/CategoryGroupListItem";
 import { Box } from "@mui/material";
 import ProductFilterDrawer from "../ProductFilterDrawer/ProductFilterDrawer";
+import { PriceFilterSlider } from "../PriceFilterSlider/PriceFilterSlider";
 
 export function CategoryFilterList({
-  category,
   onClickItem,
   loadingPage,
   selectedProductFilters,
+  setSelectedProductFilterPrice,
+  currentCategoryFilters,
+  productFilterPrice,
 }) {
-  const [currentCategoryFilters, setCurrentCategoryFilters] = useState([]);
-
-  useEffect(() => {
-    async function fetchCategoryFilters() {
-      const categoryFilters = await ProductCategoriesApi.fetchCategoryFilters(
-        category.id
-      );
-
-      if (categoryFilters.length > 0) {
-        setCurrentCategoryFilters(categoryFilters);
-      }
-    }
-    fetchCategoryFilters();
-  }, [category]);
-
   return (
     <>
       <Box sx={{ display: { md: "none" } }}>
@@ -38,6 +24,10 @@ export function CategoryFilterList({
       </Box>
       <Box sx={{ display: { xs: "none", md: "flex" } }}>
         <FormGroup>
+          <PriceFilterSlider
+            productFilterPrice={productFilterPrice}
+            setSelectedProductFilterPrice={setSelectedProductFilterPrice}
+          />
           {currentCategoryFilters.map((filter) => {
             return (
               <div key={filter.id}>
--- cheknibg-client/src/components/ProductList/ProductList.jsx
+++ cheknibg-client/src/components/ProductList/ProductList.jsx
@@ -1,58 +1,11 @@
-import { useState, useEffect } from "react";
-import { ProductCategoriesApi } from "../../api/product-categories";
 import { ProductListItem } from "../ProductListItem/ProductListItem";
 import Grid from "@mui/material/Grid";
 import { PaginationComponent } from "../Pagination/PaginationComponent";
 
-export function ProductList({ categoryFilters, selectedProductFilters }) {
-  const [currentProducts, setCurrentProducts] = useState([]);
-  const [page, setPage] = useState(1);
-
-  async function fetchProductFilters() {
-    const products = await ProductCategoriesApi.fetchProductFilters(
-      selectedProductFilters,
-      page
-    );
-
-    if (products) {
-      setCurrentProducts(products);
-    }
-  }
-
-  async function fetchCategoryProducts() {
-    const products = await ProductCategoriesApi.fetchCategoryProducts(
-      categoryFilters.id,
-      page
-    );
-
-    if (products) {
-      setCurrentProducts(products);
-    }
-  }
-
+export function ProductList({ currentProducts, setPage, page }) {
   function handlePageChange(page) {
     setPage(page);
   }
-
-  // Loads products when category filter is selected
-  useEffect(() => {
-    if (selectedProductFilters.length === 0) {
-      fetchCategoryProducts();
-      return;
-    }
-    fetchProductFilters();
-  }, [page]);
-
-  useEffect(() => {
-    handlePageChange(1);
-
-    if (selectedProductFilters.length === 0) {
-      fetchCategoryProducts();
-      return;
-    }
-
-    fetchProductFilters();
-  }, [selectedProductFilters]);
 
   return (
     <Grid container spacing={2}>
