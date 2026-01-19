088fa241f4064495512855557b8c1f0ef8453907
Tihomir Kehayov <kehayov89@gmail.com>
2026-01-19 16:37:28 +0200

    108-implement-price-sort-filter

---
+++ cheknibg-client/src/components/PriceSortProducts/PriceSortProducts.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { Grid } from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+
+const PriceSortProducts = ({ onSortNamePrice }) => {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [direction, setDirection] = useState("asc");
+  const handleSort = (event, newSort) => {
+    const effectiveSort = newSort || sortOrder;
+    console.log("Sorting by:", effectiveSort);
+    if (effectiveSort === sortOrder) {
+      const newDir = direction === "asc" ? "desc" : "asc";
+      setDirection(newDir);
+      if (onSortNamePrice) onSortNamePrice(effectiveSort, newDir);
+    } else {
+      setSortOrder(effectiveSort);
+      setDirection("asc");
+      if (onSortNamePrice) onSortNamePrice(effectiveSort, "asc");
+    }
+  };
+
+  return (
+    <Grid item>
+      <ToggleButtonGroup
+        size="small"
+        value={sortOrder}
+        exclusive
+        onChange={handleSort}
+        sx={{
+          "& .MuiToggleButton-root": {
+            color: (theme) => theme.palette.secondary.main,
+            textTransform: "none",
+            "&.Mui-selected": {
+              color: (theme) => theme.palette.secondary.main,
+              background: "transparent",
+              border: (theme) => `1px solid ${theme.palette.primary.main}`,
+            },
+          },
+        }}
+      >
+        <ToggleButton value="nameSort">
+          <strong>Име</strong>
+          {(sortOrder === "priceSort" || sortOrder === null) && (
+            <ArrowUpwardIcon fontSize="small" />
+          )}
+          {sortOrder === "nameSort" &&
+            (direction === "asc" ? (
+              <ArrowUpwardIcon fontSize="small" />
+            ) : (
+              <ArrowDownwardIcon fontSize="small" />
+            ))}
+        </ToggleButton>
+        <ToggleButton value="priceSort">
+          <strong>Цена</strong>
+          {(sortOrder === "nameSort" || sortOrder === null) && (
+            <ArrowUpwardIcon fontSize="small" />
+          )}
+          {sortOrder === "priceSort" &&
+            (direction === "asc" ? (
+              <ArrowUpwardIcon fontSize="small" />
+            ) : (
+              <ArrowDownwardIcon fontSize="small" />
+            ))}
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Grid>
+  );
+};
+
+export default PriceSortProducts;
